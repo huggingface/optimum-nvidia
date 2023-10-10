3250e96f1eae92f500e730c4594f48fdde16d90d
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-10-10 23:37:31 +0200

    Add fsspec to the requirements.

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ except Exception as error:
 
 INSTALL_REQUIRES = [
     # "transformers >= 4.32.1",
+    "fsspec",
     "huggingface_hub >= 0.14.0",
     "numpy >= 1.24.0"
     "optimum >= 1.13.0",
