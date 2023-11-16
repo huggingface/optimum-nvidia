36ef133b6831f8be42fa5630f0af6ed7a4cd8a35
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-11-16 22:45:24 +0100

    Make runtime more robust to I/O dtype

--- src/optimum/nvidia/runtime.py
+++ src/optimum/nvidia/runtime.py
@@ -1,12 +1,15 @@
 import json
+import torch
+from logging import getLogger
 from typing import Any, Dict, List, Optional, Union
 from pathlib import Path
 
-import torch
 from huggingface_hub import ModelHubMixin
 
 import tensorrt_llm.bindings as ctrrt
 
+
+LOGGER = getLogger(__name__)
 
 PackedTensor = List[torch.Tensor]
 
@@ -17,7 +20,17 @@ class TRTEnginePretrainedModel(ModelHubMixin):
 
 
 class TRTEngineForCausalLM(TRTEnginePretrainedModel):
-    __slots__ = ("_config", "_mapping", "_session", "_cache", "_use_packed_inputs", "_max_beam_width")
+    __slots__ = (
+        "_config",
+        "_mapping",
+        "_session",
+        "_use_packed_inputs",
+        "_max_beam_width",
+        "_max_batch_size",
+        "_max_prompt_length"
+        "_max_new_tokens"
+        "_max_output_length"
+    )
 
     def __init__(
         self,
@@ -54,10 +67,18 @@ class TRTEngineForCausalLM(TRTEnginePretrainedModel):
 
         # Additional cached properties
         self._use_packed_inputs = config["plugin_config"].get("remove_input_padding", False)
+        self._max_batch_size = self._config.model_config.max_batch_size
+        self._max_prompt_length = self._config.model_config.max_input_len
+        self._max_output_length = self._config.model_config.max_output_len
+        self._max_new_tokens = self._max_output_length - self._max_prompt_length
         self._max_beam_width = self._session_config.max_beam_width
 
+    @property
+    def config(self) -> ctrrt.GptJsonConfig:
+        return self._config
+
     def generate(self,
-        inputs: Union[PackedTensor, torch.Tensor],
+        input_ids: Union[PackedTensor, torch.Tensor],
         attention_mask: Optional[torch.Tensor] = None,
         max_new_tokens: int = 64,
         num_beams: int = 1,
@@ -81,33 +102,41 @@ class TRTEngineForCausalLM(TRTEnginePretrainedModel):
         generation_config.length_penalty = [length_penalty]
 
         with torch.no_grad():
-            if isinstance(inputs, torch.Tensor):
-                input_ids = inputs
+            if isinstance(input_ids, torch.Tensor):
 
                 if attention_mask is not None:
-                    lengths = attention_mask.sum(dim=0)
+                    lengths = attention_mask.sum(dim=1, dtype=torch.int32)
                 elif input_ids.ndim == 1:
-                    lengths = torch.tensor([inputs.size(0)], dtype=torch.int32)
-                elif input_ids.ndim == 2 and inputs.size(0) == 1:
+                    lengths = torch.tensor([input_ids.size(0)], dtype=torch.int32)
+                elif input_ids.ndim == 2 and input_ids.size(0) == 1:
                     lengths = torch.tensor([input_ids.size(1)], dtype=torch.int32)
                 else:
                     raise NotImplementedError("Cannot compute lengths from torch.Tensor without attention_mask")
             else:
-                input_ids = torch.nested.nested_tensor(inputs)
-                lengths = torch.tensor([tensor.size(0) for tensor in inputs], dtype=torch.int32)
+                input_ids = torch.nested.nested_tensor(input_ids)
+                lengths = torch.tensor([tensor.size(0) for tensor in input_ids], dtype=torch.int32)
+
+            if torch.any(torch.gt(lengths, self._max_prompt_length)):
+                raise ValueError(f"Input length is bigger than maximum prompt length ({self._max_prompt_length}).")
 
             trt_inputs = ctrrt.GenerationInput(
                 end_id=eos_token_id,
                 pad_id=pad_token_id,
-                ids=input_ids.view((1, -1)),
+                ids=input_ids.view((input_ids.size(0), -1)).int(),
                 lengths=lengths,
                 packed=self._use_packed_inputs
             )
 
             # Define some additional parameters based on the above
-            trt_inputs.max_new_tokens = max_new_tokens
+            if max_new_tokens > self._max_new_tokens:
+                LOGGER.warning(f"max_new_tokens {max_new_tokens} reduced to {self._max_new_tokens} to match engine.")
 
-            trt_outputs = ctrrt.GenerationOutput(ids=torch.empty(10000, dtype=torch.int32), lengths=torch.empty(10000, dtype=torch.int32))
+            trt_inputs.max_new_tokens = min(max_new_tokens, self._max_new_tokens)
+
+            trt_outputs = ctrrt.GenerationOutput(
+                ids=torch.empty((self._max_batch_size, self._max_output_length), dtype=torch.int32),
+                lengths=torch.empty(self._max_batch_size, dtype=torch.int32)
+            )
             self._session.generate(trt_outputs, trt_inputs, generation_config)
 
             return trt_outputs.ids
