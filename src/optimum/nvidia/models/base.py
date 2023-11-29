ddafc7cbe80db89dcbedb6fc0614f2d5ebaa7249
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-11-29 09:50:32 +0100

    Correct wrong typing

--- src/optimum/nvidia/models/base.py
+++ src/optimum/nvidia/models/base.py
@@ -12,6 +12,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from typing import Type
+
 from optimum.nvidia.weights import WeightAdapter
 from tensorrt_llm import Module
 
@@ -20,5 +22,5 @@ class ConvertibleModel:
     """
 
     """
-    ADAPTER: WeightAdapter
-    TARGET: Module
+    ADAPTER: Type[WeightAdapter]
+    TARGET: Type[Module]
