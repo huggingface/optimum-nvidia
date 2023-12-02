ac4a914c622faaa19b4535ceaee1ab6119433c6a
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-12-02 18:15:27 +0100

    Add guard to NVML to ensure it has been initialized

--- src/optimum/nvidia/utils/nvml.py
+++ src/optimum/nvidia/utils/nvml.py
@@ -1,10 +1,26 @@
 import functools
 from typing import Optional, Tuple
 
-from pynvml import nvmlDeviceGetHandleByIndex, nvmlDeviceGetCudaComputeCapability
+from pynvml import nvmlDeviceGetHandleByIndex, nvmlDeviceGetCudaComputeCapability, nvmlInit
+
+_NVML_INITIALIZED = False
+
+
+def nvml_guard(f):
+    @functools.wraps(f)
+    def _ensure_init(*args, **kwargs):
+        global _NVML_INITIALIZED
+        if not _NVML_INITIALIZED:
+            nvmlInit()
+            _NVML_INITIALIZED = True
+
+        return f(*args, **kwargs)
+
+    return _ensure_init
 
 
 @functools.cache
+@nvml_guard
 def get_device_compute_capabilities(device: int) -> Optional[Tuple[int, int]]:
     nvml_device_handle = nvmlDeviceGetHandleByIndex(device)
     return nvmlDeviceGetCudaComputeCapability(nvml_device_handle)
