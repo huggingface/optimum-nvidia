9424ed987dad86c538afc6aa0909e69406f93917
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-12-12 18:41:28 +0100

    Add quantization tests

---
+++ tests/quantization.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from unittest import TestCase
+
+from optimum.nvidia.configs import QuantizationConfig
+from tensorrt_llm.quantization import QuantMode
+
+
+class BaseQuantizationConfigTestCase(TestCase, ABC):
+
+    @abstractmethod
+    def test_has_quantization_step(self):
+        raise NotImplementedError()
+
+class NoQuantizationConfigTestCase(BaseQuantizationConfigTestCase):
+    """
+    Test setup for cases without quantization involved
+    """
+
+    def setUp(self):
+        self.qconfig = QuantizationConfig(QuantMode(0))
+
+    def test_has_quantization_step(self):
+        self.assertFalse(self.qconfig.has_quantization_step, "has_quantization_step should be False")
+
+
+class Float8QuantizationConfigTestCase(BaseQuantizationConfigTestCase):
+    """
+    Test setup for cases for float8 quantization
+    """
+
+    def setUp(self):
+        self.qconfig = QuantizationConfig(QuantMode.from_description(
+            use_fp8_qdq=True,
+            use_fp8_kv_cache=True
+        ))
+
+    def test_has_quantization_step(self):
+        self.assertTrue(self.qconfig.has_quantization_step)
