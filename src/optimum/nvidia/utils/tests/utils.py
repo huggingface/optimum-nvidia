93369af078ee3baecba559dcf4e87eba63b0c452
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-12-13 14:34:47 +0100

    Add utility to ensure we can run on GPU and define how much we need

--- src/optimum/nvidia/utils/tests/utils.py
+++ src/optimum/nvidia/utils/tests/utils.py
@@ -3,12 +3,15 @@ import os
 import unittest
 from distutils.util import strtobool
 
+from optimum.nvidia.utils.nvml import get_device_count
+
 
 INT_TRUE_VALUE = 1
 
 # Environment variable controlling test set
 ENVVAR_NAME_RUN_NIGHTLY = "RUN_NIGHTLY"
 ENVVAR_NAME_RUN_SLOW = "RUN_SLOW"
+ENVVAR_NAME_RUN_CPUONLY = "RUN_CPUONLY"
 
 
 @functools.cache
@@ -44,3 +47,20 @@ def slow(f):
     :return:
     """
     return unittest.skipUnless(parse_flag_from_env(ENVVAR_NAME_RUN_SLOW), "test is slow")(f)
+
+def requires_gpu(count: int = 1):
+    """
+    Ensure the unittest callable will execute on as many GPUs as `count`
+    :param count: Number of required GPUs
+    :return:
+    """
+    assert count >= 1, f"count should be >= 1, got {count}"
+    cpu_only = parse_flag_from_env(ENVVAR_NAME_RUN_CPUONLY)
+
+    def _checked_gpu_decorator(f):
+        num_gpus = get_device_count()
+        return unittest.skipUnless(
+            not cpu_only and num_gpus >= count,
+            f"Not enough GPU (requested: {count}, available: {num_gpus})"
+        )(f)
+    return _checked_gpu_decorator
