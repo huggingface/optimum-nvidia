9f65740e22bbe0e3a263a750e030f0eb234c4647
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2023-12-16 23:32:41 +0100

    Enable HF Transfer in tests (#51)
    * Enable `hf-transfer`
    
    * Add a flag for the user-agent in ci
    
    * Add --fix for ruff in fix-quality make rule

--- src/optimum/nvidia/utils/hub.py
+++ src/optimum/nvidia/utils/hub.py
@@ -5,6 +5,7 @@ from pynvml import nvmlDeviceGetCount, nvmlInit, nvmlSystemGetDriverVersion
 
 from ..version import __version__
 from .nvml import get_device_compute_capabilities
+from .tests.utils import parse_flag_from_env
 
 
 USER_AGENT_BASE = [f"optimum/nvidia/{__version__}", f"python/{pyversion.split()[0]}"]
@@ -18,6 +19,7 @@ def get_user_agent() -> str:
     """
     ua = USER_AGENT_BASE.copy()
 
+    # Nvidia driver / devices
     try:
         nvmlInit()
         ua.append(f"nvidia/{nvmlSystemGetDriverVersion()}")
@@ -36,6 +38,7 @@ def get_user_agent() -> str:
     except (RuntimeError, ImportError):
         ua.append("nvidia/unknown")
 
+    # Torch / CUDA related version, (from torch)
     try:
         from torch import __version__ as pt_version
         from torch.version import cuda, cudnn
@@ -46,7 +49,7 @@ def get_user_agent() -> str:
     except ImportError:
         pass
 
-    # TODO: Refactor later on
+    # transformers version
     try:
         from transformers import __version__ as tfrs_version
 
@@ -54,11 +57,18 @@ def get_user_agent() -> str:
     except ImportError:
         pass
 
+    # TRTLLM version
     try:
         from tensorrt_llm._utils import trt_version
 
         ua.append(f"tensorrt/{trt_version()}")
     except ImportError:
         pass
+
+    # Add a flag for CI
+    if parse_flag_from_env("OPTIMUM_NVIDIA_IS_CI", False):
+        ua.append("is_ci/true")
+    else:
+        ua.append("is_ci/false")
 
     return "; ".join(ua)
