71a8bbeea49c66ec49141615d24229f738adf051
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-12-18 15:58:37 +0100

    Added some model validation for llama build

--- tests/models/test_llama.py
+++ tests/models/test_llama.py
@@ -2,6 +2,7 @@ from typing import NamedTuple
 
 import pytest
 
+from optimum.nvidia.lang import DataType
 from optimum.nvidia.models.llama import LLamaForCausalLM as TrtLlamaForCausalLM
 from optimum.nvidia.utils.tests import requires_gpu
 
@@ -13,23 +14,29 @@ LlamaModelInfo = NamedTuple(
 
 @pytest.fixture(scope="module")
 def llama(request) -> TrtLlamaForCausalLM:
-    info: LlamaModelInfo = request.param
+    info = request.param
     yield TrtLlamaForCausalLM.from_pretrained(info.model_id, dtype=info.dtype)
 
 
 @pytest.mark.parametrize(
-    "llama",
+    "llama, dtype",
     [
-        LlamaModelInfo("huggingface/llama-7b", "float16", 1, 1, 1),
-        LlamaModelInfo("huggingface/llama-7b", "bfloat16", 1, 1, 1),
+        (LlamaModelInfo("huggingface/llama-7b", "float16", 1, 1, 1), "float16"),
+         (LlamaModelInfo("huggingface/llama-7b", "bfloat16", 1, 1, 1), "bfloat16"),
     ],
     ids=[
         "huggingface/llama-7b (float16)",
         "huggingface/llama-7b (bfloat16)",
     ],
-    indirect=True,
+    indirect=["llama"],
 )
 @requires_gpu
-def test_build_llama(llama: TrtLlamaForCausalLM):
+def test_build_llama(llama: TrtLlamaForCausalLM, dtype: str):
     assert llama is not None
     assert llama.config.name == "llama"
+    assert llama.config.precision == dtype, f"Precision should be {dtype} but got {llama.config.precision}"
+
+    model_config = llama.config
+    assert model_config.data_type == DataType(dtype).as_trt()
+    assert model_config.use_gpt_attention_plugin, f"GPT Attention plugin should be enabled"
+    assert model_config.use_packed_input, f"Remove Padding should set to true with GPT Attention plugin"
