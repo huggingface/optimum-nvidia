71d8da31b9ce8f892b2f2a54570c50a2c9f6e62d
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-02-26 11:49:20 +0100

    Update license

--- src/optimum/nvidia/pipelines/text_generation.py
+++ src/optimum/nvidia/pipelines/text_generation.py
@@ -1,3 +1,18 @@
+#  coding=utf-8
+#  Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import warnings
 from enum import Enum
 from typing import Dict, List, Union
@@ -5,7 +20,8 @@ from typing import Dict, List, Union
 import torch
 from transformers import PreTrainedTokenizer, TensorType
 
-from optimum.nvidia import AutoModelForCausalLM, TensorRTForCausalLM
+from optimum.nvidia import AutoModelForCausalLM
+from optimum.nvidia.runtime import CausalLM
 
 from .base import Pipeline
 
@@ -21,7 +37,7 @@ class TextGenerationPipeline(Pipeline):
 
     __slots__ = ("tokenizer", "_runtime", "_bos_token_id", "_eos_token_id", "_pad_token_id")
 
-    def __init__(self, model: TensorRTForCausalLM, tokenizer: PreTrainedTokenizer):
+    def __init__(self, model: CausalLM, tokenizer: PreTrainedTokenizer):
         if tokenizer.eos_token and not tokenizer.pad_token:
             tokenizer.pad_token = tokenizer.eos_token
 
