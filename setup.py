f29320b9626600950573733c0f10a276ee6530a3
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-02-28 22:04:58 +0100

    Ping huggingface_hub to 0.20.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except Exception as error:
 INSTALL_REQUIRES = [
     "accelerate",
     "dataset",
-    "huggingface_hub >= 0.14.0",
+    "huggingface_hub == 0.20.0",
     "hf-transfer",
     "numpy >= 1.26.0",
     "onnx >= 1.12.0",
