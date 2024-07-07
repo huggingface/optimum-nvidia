5ac3547c7f4e647b0d9a1b2929d1947a1df067a8
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-07-07 23:28:57 +0200

    Ensure torch dependency constraints match TRTLLM

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ INSTALL_REQUIRES = [
     "optimum >= 1.13.0",
     "setuptools",
     "tensorrt-llm == 0.12.0.dev2024070200",
-    "torch>=2.2.0a,<=2.3.0a",
+    "torch>=2.3.0a,<=2.4.0a",
     "transformers >= 4.38.2",
     "pynvml"
 ]
