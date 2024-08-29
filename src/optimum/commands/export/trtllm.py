899d24a805681993103f0e529ace442eef5eaf74
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-08-29 11:53:00 +0200

    (feat) add main.py to exporter for trtllm

--- src/optimum/commands/export/trtllm.py
+++ src/optimum/commands/export/trtllm.py
@@ -3,55 +3,11 @@ import sys
 from typing import TYPE_CHECKING, Optional
 
 from ..base import BaseOptimumCLICommand, CommandInfo
-
+from ...nvidia.export.cli import common_trtllm_export_args
 
 if TYPE_CHECKING:
     from argparse import ArgumentParser, Namespace, _SubParsersAction
 
-
-def parse_args_trtllm(parser: "ArgumentParser"):
-    required_group = parser.add_argument_group("Required arguments")
-    required_group.add_argument(
-        "-m",
-        "--model",
-        type=str,
-        required=True,
-        help="Model ID on huggingface.co or path on disk to load model from.",
-    )
-    required_group.add_argument(
-        "--max-input-length",
-        type=int,
-        default=1,
-        help="Maximum sequence length, in number of tokens, the prompt can be. The maximum number of potential tokens "
-        "generated will be <max-output-length> - <max-input-length>.",
-    )
-    required_group.add_argument(
-        "--max-output-length",
-        type=int,
-        default=1,
-        help="Maximum sequence length, in number of tokens, the model supports.",
-    )
-
-    optional_group = parser.add_argument_group("Optional arguments")
-    optional_group.add_argument(
-        "-d",
-        "--dtype",
-        type=str,
-        default="auto",
-        help="Computational data type used for the model.",
-    )
-    optional_group.add_argument(
-        "--max-batch-size",
-        type=int,
-        default=1,
-        help="Maximum number of concurrent requests the model can process.",
-    )
-    optional_group.add_argument(
-        "--max-beams-width",
-        type=int,
-        default=1,
-        help='Maximum number of sampling paths ("beam") to evaluate when decoding new a token.',
-    )
 
 
 class TrtLlmExportCommand(BaseOptimumCLICommand):
@@ -78,8 +34,8 @@ class TrtLlmExportCommand(BaseOptimumCLICommand):
 
     @staticmethod
     def parse_args(parser: "ArgumentParser"):
-        return parse_args_trtllm(parser)
+        return common_trtllm_export_args(parser)
 
     def run(self):
-        full_command = f"python3 -m optimum.exporters.neuron {self.args_string}"
+        full_command = f"python3 -m optimum.exporters.trtllm {self.args_string}"
         subprocess.run(full_command, shell=True, check=True)
---
+++ src/optimum/nvidia/export/cli.py
@@ -0,0 +1,43 @@
+def common_trtllm_export_args(parser: "ArgumentParser"):
+    required_group = parser.add_argument_group("Required arguments")
+    required_group.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        required=True,
+        help="Model ID on huggingface.co or path on disk to load model from.",
+    )
+    required_group.add_argument(
+        "--max-input-length",
+        type=int,
+        default=1,
+        help="Maximum sequence length, in number of tokens, the prompt can be. The maximum number of potential tokens "
+             "generated will be <max-output-length> - <max-input-length>.",
+    )
+    required_group.add_argument(
+        "--max-output-length",
+        type=int,
+        default=1,
+        help="Maximum sequence length, in number of tokens, the model supports.",
+    )
+
+    optional_group = parser.add_argument_group("Optional arguments")
+    optional_group.add_argument(
+        "-d",
+        "--dtype",
+        type=str,
+        default="auto",
+        help="Computational data type used for the model.",
+    )
+    optional_group.add_argument(
+        "--max-batch-size",
+        type=int,
+        default=1,
+        help="Maximum number of concurrent requests the model can process.",
+    )
+    optional_group.add_argument(
+        "--max-beams-width",
+        type=int,
+        default=1,
+        help='Maximum number of sampling paths ("beam") to evaluate when decoding new a token.',
+    )
