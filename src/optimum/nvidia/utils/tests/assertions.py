6aaa0450b271fa516d15ff43aa860b08a7632ec7
Morgan Funtowicz <funtowiczmo@gmail.com>
2025-01-15 09:33:13 +0100

    misc(trtllm): quality

--- src/optimum/nvidia/utils/tests/assertions.py
+++ src/optimum/nvidia/utils/tests/assertions.py
@@ -13,9 +13,9 @@ def assert_generated_partially_match(
     matched = np.sum(generated[:max_overlap] == gold[:max_overlap])
     ratio = matched / max_overlap
 
-    assert (
-        1.0 - ratio
-    ) < atol, f"generated contact overlapping items is below tolerance {atol} ({ratio})"
+    assert (1.0 - ratio) < atol, (
+        f"generated contact overlapping items is below tolerance {atol} ({ratio})"
+    )
 
 
 def assert_generated_text_partially_match(generated: str, gold: str, atol: float):
@@ -26,6 +26,6 @@ def assert_generated_text_partially_match(generated: str, gold: str, atol: float
     matched = np.sum(generated[:max_overlap] == gold[:max_overlap])
     ratio = matched / max_overlap
 
-    assert (
-        1.0 - ratio
-    ) < atol, f"generated contact overlapping items is below tolerance {atol} ({ratio})"
+    assert (1.0 - ratio) < atol, (
+        f"generated contact overlapping items is below tolerance {atol} ({ratio})"
+    )
